d27dcc129b6050e8dfcd37ec731543f4223edd74
limin <2662047696@qq.com>
2020-04-15 17:18:08 +0800

    增加请求fetch  和封装的方法 以及显示数据 for循环

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+<!-- git remote add origin 地址 -->
+
 ## Available Scripts
 
 In the project directory, you can run:
---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    baseUrlDev : 'https://www.guomanbang.com', // 测试路径
+    baseImgDev : 'http://223.93.144.103:9090/',//测试图片路径
+    baseUrlPro : 'https://www.guomanbang.com', // 正式路径
+    baseUrlImg : 'http://cdn.guomanbang.com/', //正式图片地址
+    
+    commonUrlImg: this.baseImgDev,//页面使用 图片地址，默认是测试的
+    socketUrl : "wss://admin.guomanmeng.com:9000/",
+    
+    aMapKey : '8a20d5110d6f550eeba6ebfb972f8e9e' // 高德地图key
+  }
